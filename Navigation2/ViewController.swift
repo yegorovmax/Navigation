4ba6a07d01f40f930b78cf6c852ecd0c7fd45247
Max Egorov <yegorovmax@yahoo.com>
2022-02-06 21:58:19 -0800

    Initial Commit

---
+++ Navigation2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation2
+//
+//  Created by Maksim Egorov on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
